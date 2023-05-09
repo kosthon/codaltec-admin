98c79afb8ef9c895c396335275ec6ebe96f806d4
Alejandro F <nforero@codaltec.com>
2023-05-09 16:35:33 -0500

    ⚙ fixed bugs ProductsPage

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,8 +1,26 @@
 import { Helmet } from 'react-helmet-async';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+
 // @mui
-import { Card, Table, Stack, Button, TableBody, Container, Typography, TableContainer } from '@mui/material';
+import {
+  Card,
+  Table,
+  Stack,
+  Button,
+  TableBody,
+  Container,
+  Typography,
+  TableContainer,
+  Box,
+  Modal,
+  Input,
+  FormControl,
+  InputLabel,
+  FormHelperText,
+  TextField,
+} from '@mui/material';
 import { UserListHead } from '../sections/@dashboard/user';
 // components
 import Scrollbar from '../components/scrollbar';
@@ -20,6 +38,30 @@ const TABLE_HEAD = [
   { id: '' },
 ];
 
+const style = {
+  display: 'flex',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  minWidth: 300,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  borderRadius: 4,
+  flexDirection: 'column',
+};
+const boxForm = {
+  display: 'flex',
+  flexDirection: 'column',
+};
+const formControl = {
+  margin: 1,
+};
+
 // ----------------------------------------------------------------------
 
 export default function ProductsPage() {
@@ -27,11 +69,72 @@ export default function ProductsPage() {
   const [portfolio, setPortfolio] = useState([]);
   const [requestData, setRequestData] = useState(new Date());
 
+  const [updateTable, setUpdateTable] = useState(new Date());
+
+  const handleCreatProductSuccess = () => {
+    setUpdateTable(new Date());
+  };
+
   useEffect(() => {
     axios.get(url).then((response) => {
       setPortfolio(response.data);
     });
   }, [requestData]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpenMenu = (event) => {
+    setOpen(!false);
+  };
+  const handleCloseMenu = () => {
+    setOpen(false);
+  };
+
+  const [titleEsp, setTitleEsp] = useState('');
+  const [titleEng, setTitleEng] = useState('');
+  const [despEsp, setDespEsp] = useState('');
+  const [despEng, setDespEng] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const dataFile = new FormData();
+
+    dataFile.append('esName', titleEsp);
+    dataFile.append('enName', titleEng);
+    dataFile.append('esDescription', despEsp);
+    dataFile.append('enDescription', despEng);
+
+    await axios
+      .post('https://codaltec-api.website:3000/api/v1/business', dataFile)
+      .then((response) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Elemento creado',
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        console.log(response);
+        handleCreatProductSuccess();
+        setOpen(false);
+        resetInputs();
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: 'warning',
+          title: 'Por favor digite datos validos',
+          showConfirmButton: true,
+        });
+      });
+  };
+
+  const resetInputs = () => {
+    setTitleEsp('');
+    setTitleEng('');
+    setDespEsp('');
+    setDespEng('');
+  };
+
+
   return (
     <>
       <Helmet>
@@ -43,9 +146,60 @@ export default function ProductsPage() {
           <Typography variant="h4" gutterBottom>
             Portafolio
           </Typography>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleOpenMenu}>
             Crear Linea de negocio
           </Button>
+
+          <Modal open={open !== null && open} onClose={() => setOpen(false)}>
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h4" component="h1">
+                Ingrese los campos para crear una línea de
+              </Typography>
+              <form method="POST">
+                <Box sx={boxForm}>
+                  <FormControl sx={formControl}>
+                    <InputLabel htmlFor="my-input">Título en Español</InputLabel>
+                    <Input type="text" name="esName" onChange={(e) => setTitleEsp(e.target.value)} />
+                  </FormControl>
+
+                  <FormControl sx={formControl}>
+                    <InputLabel htmlFor="my-input">Título en Ingles</InputLabel>
+                    <Input type="text" name="enName" onChange={(e) => setTitleEng(e.target.value)} />
+                  </FormControl>
+
+                  <FormControl sx={formControl}>
+                    <TextField
+                      label="Descripción en Español"
+                      multiline
+                      rows={4}
+                      name="despEsp"
+                      onChange={(e) => setDespEsp(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormControl sx={formControl}>
+                    <TextField
+                      label="Descripción en Ingles"
+                      multiline
+                      rows={4}
+                      name="despEng"
+                      onChange={(e) => setDespEng(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <Stack spacing={1}>
+                    <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={onSubmitForm}>
+                      Guardar
+                    </Button>
+                    <Button variant="outlined" onClick={handleCloseMenu}>
+                      Cerrar
+                    </Button>
+                  </Stack>
+                </Box>
+              </form>
+            </Box>
+          </Modal>
+
         </Stack>
 
         <Card>
